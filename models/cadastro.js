cd4d21b88374dec8da18d2013e5d433cf3b31859
Mateus Goncalves de Oliveira <mateus.disp@gmail.com>
2017-08-27 04:06:09 -0300

    pequenas alteracoes na alteracao do quadro da manutencao. inseridas funcionalidades de mask em jquery para forms. pagina de cadastro melhorada, de forma identica a original do site do icea, mas ainda sem o captcha

--- models/cadastro.js
+++ models/cadastro.js
@@ -3,10 +3,19 @@ var mongoose = require('mongoose');
 
 module.exports = mongoose.model('Cadastro',{
 	name: String,
+	name_guerra: String,
+	saram: String,
+	identidade: String,
+	unidade: String,
+	endereco: String,
+	telefone: String,
+	CPF: String,
 	email: String,
 	cpf: String,
 	dateIn: String,
 	dateOut: String,
 	acompanhante: Number,
 	posto: Number,
+	curso: String,
+	solicitante: String
 });
